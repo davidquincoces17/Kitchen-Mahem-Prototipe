52868284aa7ef93fb2274446cd37da964df5a1ff
davidquincoces17 <david.quincoces01@estudiant.upf.edu>
2021-06-07 18:20:09 +0200

    fireeee

--- Assets/scripts/FireLight.cs
+++ Assets/scripts/FireLight.cs
@@ -11,7 +11,7 @@ public class FireLight : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //this.gameObject.GetComponent<Light>().enabled = false;
+
     }
 
     // Update is called once per frame
