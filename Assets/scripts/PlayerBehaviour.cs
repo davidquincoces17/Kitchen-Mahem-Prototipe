d48f91ddcf5dcc787c761a314daedcbd70c4d20e
quimHM <83378509+quimHM@users.noreply.github.com>
2021-06-09 12:36:13 +0200

    Interaction Timers

--- Assets/scripts/PlayerBehaviour.cs
+++ Assets/scripts/PlayerBehaviour.cs
@@ -8,11 +8,14 @@ public class PlayerBehaviour : MonoBehaviour
     public FoodObject inHand;
     public FireExtinguisher inHandFE; // fire extinguisher
     public Order inHandO; // fire extinguisher
-
+    public TimerInteraction interT;
+    public GameObject timerPrefab;
+    public Collider other;
+   
     // Start is called before the first frame update
     void Start()
     {
-
+	interT = Instantiate(timerPrefab, transform.position, timerPrefab.transform.rotation, GameObject.FindGameObjectWithTag("Canvas").transform).GetComponent<TimerInteraction>();   
     }
 
     // Update is called once per frame
@@ -30,50 +33,134 @@ public class PlayerBehaviour : MonoBehaviour
         {
             inHandO.transform.position = transform.position + Vector3.up;
         } 
-        
+	interT.transform.position = transform.position + Vector3.up*3;
+	checkInteraction();
+	//other = null;
+	if(interT.state != 0){interT.state=-1;}
     }
-
-    private void OnTriggerEnter(Collider other)
+    private void beginInteraction(Collider c_other){
+	other = c_other;
+	if(interT.state == -1){interT.Begin(interT.Duration);}
+    }
+    
+    private void OnTriggerEnter(Collider c_other)
     {
-        if (!inHandFE && !inHandO)
-        {
-            if (other.CompareTag("PastaFridge") || other.CompareTag("MeatFridge") || other.CompareTag("VegetableFridge") )
+	/*if (!inHandFE && !inHandO)
             {
-                if (inHand != null)
-                {
-                    Destroy(inHand.gameObject);
-                    inHand = null;
-                    Debug.Log("Left");
-                }
-                Fridge fridge = other.gameObject.GetComponent<Fridge>();
-                inHand = fridge.spawnIngredient().GetComponent<FoodObject>();
-                //Destroy(gameObject);
-                Debug.Log("Grabbed");
+            	if (other.CompareTag("PastaFridge") || other.CompareTag("MeatFridge") || other.CompareTag("VegetableFridge") )
+            	{
+                    beginInteraction(c_other);
+            	}
+            	else if (other.CompareTag("Oven"))
+            	{
+                	OvenFire fire = other.gameObject.GetComponent<OvenFire>();
+                	if (inHand)
+                	{
+                    		if (inHand.state == 0 && !fire.inOven)
+                    		{
+                        		beginInteraction(c_other);
+                    		}
+                	}
+                	else if (fire.inOven)
+                	{
+                    		if (fire.inOven.state > 0 && !fire.fire.isPlaying)
+                    		{
+                       			beginInteraction(c_other);
+                    		}
+               		 }
             }
-            else if (other.CompareTag("Oven"))
+            else if (other.CompareTag("Counter") && inHand)
+            {
+                beginInteraction(c_other);
+            }
+
+            else if (other.CompareTag("FireExtinguisher") && !inHandO)
+            {
+               beginInteraction(c_other);
+            }
+	    
+	    else if (other.CompareTag("OrderSpace"))
+            {
+		OrderHolder holder = other.gameObject.GetComponent<OrderHolder>();
+		if(holder.order){
+                	beginInteraction(c_other);
+		}
+		else{Debug.Log("no order");}
+            }
+        } 
+	else if (!inHandFE && inHandO)
+        {
+	    if (other.CompareTag("Counter"))
+            {
+                beginInteraction(c_other);
+            }
+	}
+        else if (inHandFE && !inHandO)
+        {
+            if (other.CompareTag("Oven"))
             {
                 OvenFire fire = other.gameObject.GetComponent<OvenFire>();
-                if (inHand)
-                {
-                    if (inHand.state == 0 && !fire.inOven)
-                    {
-                        fire.inOven = inHand;
-                        fire.inOven.transform.position = fire.transform.position + new Vector3(0, 15, 0);
-                        inHand = null;
 
-                        Debug.Log("Cooking", other);
-                        fire.startCooking();
+                if (fire.inOven)
+                {
+                    if (fire.inOven.state == 2)
+                    {
+                        beginInteraction(c_other);
                     }
                 }
-                else if (fire.inOven)
-                {
-                    if (fire.inOven.state > 0 && !fire.fire.isPlaying)
-                    {
-                        inHand = fire.inOven;
-                        fire.inOven = null;
-                        Destroy(fire.timer.gameObject);
+            }
+        }*/
+	beginInteraction(c_other);
+    }
 
-                        // Removing smoke
+    private void OnTriggerExit(Collider c_other)
+    {
+	other = null;
+	interT.state = -1;
+Debug.Log("out");
+    }
+
+    private void checkInteraction(){
+	if(other && interT.state == 1){
+            if (!inHandFE && !inHandO)
+            {
+            	if (other.CompareTag("PastaFridge") || other.CompareTag("MeatFridge") || other.CompareTag("VegetableFridge") )
+            	{
+                    if (inHand != null)
+                    {
+                    	Destroy(inHand.gameObject);
+                    	inHand = null;
+                    	Debug.Log("Left");
+		    }
+                    Fridge fridge = other.gameObject.GetComponent<Fridge>();
+                    inHand = fridge.spawnIngredient().GetComponent<FoodObject>();
+                    //Destroy(gameObject);
+                    Debug.Log("Grabbed");
+            	}
+            	else if (other.CompareTag("Oven"))
+            	{
+                	OvenFire fire = other.gameObject.GetComponent<OvenFire>();
+                	if (inHand)
+                	{
+                    		if (inHand.state == 0 && !fire.inOven)
+                    		{
+                        		fire.inOven = inHand;
+                        		fire.inOven.transform.position = fire.transform.position + new Vector3(0, 15, 0);
+                        		inHand = null;
+
+                        		Debug.Log("Cooking", other);
+                        		fire.startCooking();
+                    		}
+                	}
+                	else if (fire.inOven)
+                	{
+                    		if (fire.inOven.state > 0 && !fire.fire.isPlaying)
+                    		{
+                       			inHand = fire.inOven;
+                        		fire.inOven = null;
+                        		Destroy(fire.timer.gameObject);
+
+                        		// Removing smoke
                         fire.smoke.gameObject.GetComponent<ParticleSystem>().Stop();
                     }
                 }
@@ -157,5 +244,7 @@ public class PlayerBehaviour : MonoBehaviour
                 }
             }
         }
+	interT.state=-1;
+	}
     }
 }
