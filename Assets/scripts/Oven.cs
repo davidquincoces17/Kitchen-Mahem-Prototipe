bef7fb1a06cf4bb28e9010402a5dc35eba0fd54d
davidquincoces17 <david.quincoces01@estudiant.upf.edu>
2021-06-12 19:35:29 +0200

    dish sprites

--- Assets/scripts/Oven.cs
+++ Assets/scripts/Oven.cs
@@ -9,7 +9,7 @@ public class Oven : MonoBehaviour
     
     //public OvenFire Circle;
     public int tech;
-    Color32[] ColorTech = new Color32[] {new Color32(255, 255, 255, 255),new Color32(200, 200, 0, 255),new Color32(255, 25, 25, 255),new Color32(0, 25, 200, 255)};
+    Color32[] ColorTech = new Color32[] {new Color32(255, 255, 255, 50),new Color32(200, 200, 0, 50),new Color32(255, 25, 25, 100),new Color32(0, 25, 200, 50)};
     
     // Start is called before the first frame update
     void Start()
